d359234373fc9d1f67d751384a1e3e33812b052b
feehe21 <33964166+feehe21@users.noreply.github.com>
2023-02-24 18:33:52 -0500

    Update partc.c

--- partc.c
+++ partc.c
@@ -14,86 +14,36 @@
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
+#define F_CPU 16000000UL
+#define BAUD_RATE 9600
+#define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
+
 int mode = 0;
+int x = 0;
 //mode 0 = nothing
 //mode 1 = outputting
 //mode 2 = looking for rising edge of input
 //mode 3 = looking for falling edge of input
 long timeStart = 0;
+long timeEnd = 0;
 int numOverflows = 0;
 char String[25];
+int distanceCM = 0;
 
-void Initialize() {
-	cli(); //disable global interrupts for setup
-	
-	//will use this pin for both input/output
-	DDRB |= (1<<DDB0); //set to output pin (PB0=ICP1=PCINT0 -input capture for timer 1)
-	PORTB |= (1<<PORTB0); // set initially to low
-	
-	//timer 1 setup with pre scaler of 8
-	TCCR1B &= ~(1<<CS10);
-	TCCR1B |= (1<<CS11);
-	TCCR1B &= ~(1<<CS12);
-	//need to measure between 116 and 23200 us
-	//prescale by 8 so each tick is 0.5us
-	//16 * 10^6 / 8 / 2^16 = 30.5 ticks per overflow
-	//16 * 10^6 / 8 / 23200 = 86.2 ticks in 23200us
-	
-	//PCMSK0 |= (1<<PCINT0);//enable pin change interrupt
-
-	sei();//enable global interrupts
-}
-void output() {
-	cli();
-	mode = 1;
-	DDRB |= (1<<DDB0); //set to output pin
-	PORTB |= (1<<PORTB0);
-	_delay_us(10);
-	PORTB &= ~(1<<PORTB0);
-	mode = 2;
-	inputSetup();
-	sei();
-}
-void inputSetup() {
-	DDRB &= ~(1<<DDB0); //set to input pin
-	TCCR1B |= (1<<ICES1);//looking for rising edge
-	TIFR1 |= (1<<ICF1);//clear interrupt flag
-	TIMSK1 |= (1<<ICIE1);//enable input capture interrupt
-}
-ISR(TIMER1_CAPT_vect) {
-	if (mode == 2) {
-		timeStart = ICR1;
-		TCCR1B &= ~(1<<ICES1);//looking for falling edge
-	} else {
-		TIMSK1 &= ~(1<<ICIE1);//disable input capture interrupt
-		long overflowTicks = numOverflows * 65536;
-		long tickDiff = ICR1 - timeStart + overflowTicks;
-		long timeDiff = tickDiff * 8 / 16000000;
-		int distanceCM = timeDiff / 58;
-		print(distanceCM);
-		_delay_ms(10);
-		output();
-	}
-}
-ISR(TIMER1_OVF_vect) {
-	numOverflows++;
-}
-int main(void)
-{
-	Initialize();
-	//output();
-	print_init();
-	while (1) {
-		sprintf(String, "hello \n");
-		print(String);
-		_delay_ms(1000);
-	}
-}
 void print_init() {
-	int baudRate = 9600;
-	unsigned long Fcpu = 16000000UL;
-	long baudPrescaler = (((Fcpu / (baudRate * 16UL))) - 1)
-	UBRR0H = (unsigned char)(baudPrescaler >> 8);
+	sprintf(String, "print_init \n");
+	print(String);
+	UBRR0H = (unsigned char)(BAUD_PRESCALER >> 8);
+	UBRR0L = (unsigned char)BAUD_PRESCALER;
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
+	UCSR0C |= (1<<USBS0);
+}
+void printChar(unsigned char c) {
+	//waif for empty transmit buffer
+	while (!(UCSR0A & (1<<UDRE0)));
+	//send data through buffer
+	UDR0 = c;
 }
 void print(char* StringToPrint) {
 	while (*StringToPrint != 0x00) {
@@ -101,9 +51,91 @@ void print(char* StringToPrint) {
 		StringToPrint++;
 	}
 }
-void printChar(unsigned char c) {
-	//waif for empty transmit buffer
-	while (!(UCSR0A & (1<<UDRE0)));
-	//send data through buffer
-	UDR0 = c;
+
+void Initialize() {
+	sprintf(String, "initialize \n");
+	print(String);
+	_delay_ms(100);
+	cli(); //disable global interrupts for setup
+	
+	//pin 8 is the input - echo - B0  (PB0=ICP1=PCINT0 -input capture for timer 1)
+	//pin 7 is the output - trig - D7
+	
+	DDRD |= (1<<DDD7); //set to output pin
+	PORTD &= ~(1<<PORTD7); // set initially to low
+	
+	DDRB &= ~(1<<DDB0); //set to input pin
+	PORTB &= ~(1<<PORTB0);
+	
+	//timer 1 setup with pre scaler of 8
+	TCCR1B &= ~(1<<CS10);
+	TCCR1B |= (1<<CS11);
+	TCCR1B &= ~(1<<CS12);
+	
+	//set timer to normal
+	TCCR1A &= ~(1<<WGM10);
+	TCCR1A &= ~(1<<WGM11);
+	TCCR1B &= ~(1<<WGM12);
+	TCCR1B &= ~(1<<WGM13);
+	
+	TCCR1B |= (1<<ICES1);//looking for rising edge
+	TIFR1 |= (1<<ICF1);//clear interrupt flag
+	TIMSK1 |= (1<<ICIE1);//enable input capture interrupt
+	
+	TCCR1A |= (1<<COM1A0);
+	TCCR1A &= ~(1<<COM1A1);
+
+	sei();//enable global interrupts
+}
+ISR(TIMER1_CAPT_vect) {
+	if (mode != 0) {
+		x ++;
+		if (TCCR1B & (1 << ICES1)) {
+			TCNT1 = 0;
+			//timeStart = TCNT1;
+			numOverflows = 0;
+			mode = 1;
+			_delay_us(10);
+			TCCR1B &= ~(1<<ICES1);//looking for falling edge
+		} else {
+			timeEnd = TCNT1;
+			mode = 0;
+			TCCR1B |= (1<<ICES1);//rising edge
+		}
+	} else {
+		x--;
+	}
+	
+}
+ISR(TIMER1_OVF_vect) {
+	numOverflows++;
+}
+int main(void)
+{
+	print_init();
+	Initialize();
+	while (1) {
+		//sprintf(String, "%u mode \n", mode);
+		//print(String);
+		if (mode == 0) {
+			//cli();
+			PORTD |= (1<<PORTD7);//output high
+			_delay_us(5000);
+			PORTD &= ~(1<<PORTD7);//output low
+			mode = 1;
+			//sprintf(String, "%u :timeEnd \n", timeEnd);
+			//print(String);
+			//sprintf(String, "%u :TCNT1 \n", TCNT1);
+			//print(String);
+			//sei();
+		}
+		int distance = (64 * (timeEnd) * 8) / 1024 / 58;
+		/*sprintf(String, "%u :x \n", x);
+		print(String);
+		sprintf(String, "%u :overflows \n", numOverflows);
+		print(String);*/
+		sprintf(String, "%u :distance \n", distance);
+		print(String);
+		_delay_ms(1000);
+	}
 }
