8076a55d359638c18c2f2c015cc1e3bfcda3ce29
feehe21 <33964166+feehe21@users.noreply.github.com>
2023-02-27 18:50:51 -0500

    Update hold.c

--- hold.c
+++ hold.c
@@ -14,87 +14,138 @@
 #include <stdbool.h>
 #include <avr/interrupt.h>
 
-#define F_CPU 16000000UL
-#define BAUD_RATE 9600
-#define BAUD_PRESCALER (((F_CPU / (BAUD_RATE * 16UL))) - 1)
-
-int mode = 0;
 int x = 0;
-long timeStart = 0;
-long timeEnd = 0;
-int numOverflows = 0;
-char String[25];
-int distanceCM = 0;
-
-bool discrete = 1;
-int discrete_freqs[8] = {30, 32, 35, 40, 45, 47, 53, 60};
-
-void print_init() {
-	sprintf(String, "print_init \n");
-	print(String);
-	UBRR0H = (unsigned char)(BAUD_PRESCALER >> 8);
-	UBRR0L = (unsigned char)BAUD_PRESCALER;
-	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
-	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
-	UCSR0C |= (1<<USBS0);
-}
-void printChar(unsigned char c) {
-	//waif for empty transmit buffer
-	while (!(UCSR0A & (1<<UDRE0)));
-	//send data through buffer
-	UDR0 = c;
-}
-void print(char* StringToPrint) {
-	while (*StringToPrint != 0x00) {
-		printChar(*StringToPrint);
-		StringToPrint++;
-	}
-}
+int sine[1000] = {128,128,129,130,131,132,132,133,
+	134,135,136,136,137,138,139,139,
+	140,141,142,143,143,144,145,146,
+	147,147,148,149,150,151,151,152,
+	153,154,155,155,156,157,158,158,
+	159,160,161,162,162,163,164,165,
+	165,166,167,168,168,169,170,171,
+	171,172,173,174,174,175,176,177,
+	177,178,179,180,180,181,182,183,
+	183,184,185,185,186,187,188,188,
+	189,190,190,191,192,192,193,194,
+	194,195,196,196,197,198,199,199,
+	200,200,201,202,202,203,204,204,
+	205,206,206,207,208,208,209,209,
+	210,211,211,212,212,213,214,214,
+	215,215,216,217,217,218,218,219,
+	219,220,220,221,222,222,223,223,
+	224,224,225,225,226,226,227,227,
+	228,228,229,229,230,230,231,231,
+	232,232,233,233,233,234,234,235,
+	235,236,236,236,237,237,238,238,
+	238,239,239,240,240,240,241,241,
+	241,242,242,243,243,243,244,244,
+	244,245,245,245,245,246,246,246,
+	247,247,247,247,248,248,248,249,
+	249,249,249,249,250,250,250,250,
+	251,251,251,251,251,252,252,252,
+	252,252,252,253,253,253,253,253,
+	253,253,254,254,254,254,254,254,
+	254,254,254,254,255,255,255,255,
+	255,255,255,255,255,255,255,255,
+	255,255,255,255,255,255,255,255,
+	255,255,255,255,255,255,255,255,
+	255,254,254,254,254,254,254,254,
+	254,254,254,253,253,253,253,253,
+	253,253,252,252,252,252,252,252,
+	251,251,251,251,251,250,250,250,
+	250,249,249,249,249,249,248,248,
+	248,247,247,247,247,246,246,246,
+	245,245,245,245,244,244,244,243,
+	243,243,242,242,241,241,241,240,
+	240,240,239,239,238,238,238,237,
+	237,236,236,236,235,235,234,234,
+	233,233,233,232,232,231,231,230,
+	230,229,229,228,228,227,227,226,
+	226,225,225,224,224,223,223,222,
+	222,221,220,220,219,219,218,218,
+	217,217,216,215,215,214,214,213,
+	212,212,211,211,210,209,209,208,
+	208,207,206,206,205,204,204,203,
+	202,202,201,200,200,199,199,198,
+	197,196,196,195,194,194,193,192,
+	192,191,190,190,189,188,188,187,
+	186,185,185,184,183,183,182,181,
+	180,180,179,178,177,177,176,175,
+	174,174,173,172,171,171,170,169,
+	168,168,167,166,165,165,164,163,
+	162,162,161,160,159,158,158,157,
+	156,155,155,154,153,152,151,151,
+	150,149,148,147,147,146,145,144,
+	143,143,142,141,140,139,139,138,
+	137,136,136,135,134,133,132,132,
+	131,130,129,128,128,127,126,125,
+	124,123,123,122,121,120,119,119,
+	118,117,116,116,115,114,113,112,
+	112,111,110,109,108,108,107,106,
+	105,104,104,103,102,101,100,100,
+	99,98,97,97,96,95,94,93,
+	93,92,91,90,90,89,88,87,
+	87,86,85,84,84,83,82,81,
+	81,80,79,78,78,77,76,75,
+	75,74,73,72,72,71,70,70,
+	69,68,67,67,66,65,65,64,
+	63,63,62,61,61,60,59,59,
+	58,57,56,56,55,55,54,53,
+	53,52,51,51,50,49,49,48,
+	47,47,46,46,45,44,44,43,
+	43,42,41,41,40,40,39,38,
+	38,37,37,36,36,35,35,34,
+	33,33,32,32,31,31,30,30,
+	29,29,28,28,27,27,26,26,
+	25,25,24,24,23,23,22,22,
+	22,21,21,20,20,19,19,19,
+	18,18,17,17,17,16,16,15,
+	15,15,14,14,14,13,13,12,
+	12,12,11,11,11,10,10,10,
+	10,9,9,9,8,8,8,8,
+	7,7,7,6,6,6,6,6,
+	5,5,5,5,4,4,4,4,
+	4,3,3,3,3,3,3,2,
+	2,2,2,2,2,2,1,1,
+	1,1,1,1,1,1,1,1,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,
+	0,0,0,0,0,1,1,1,
+	1,1,1,1,1,1,1,2,
+	2,2,2,2,2,2,3,3,
+	3,3,3,3,4,4,4,4,
+	4,5,5,5,5,6,6,6,
+	6,6,7,7,7,8,8,8,
+	8,9,9,9,10,10,10,10,
+	11,11,11,12,12,12,13,13,
+	14,14,14,15,15,15,16,16,
+	17,17,17,18,18,19,19,19,
+	20,20,21,21,22,22,22,23,
+	23,24,24,25,25,26,26,27,
+	27,28,28,29,29,30,30,31,
+	31,32,32,33,33,34,35,35,
+	36,36,37,37,38,38,39,40,
+	40,41,41,42,43,43,44,44,
+	45,46,46,47,47,48,49,49,
+	50,51,51,52,53,53,54,55,
+	55,56,56,57,58,59,59,60,
+	61,61,62,63,63,64,65,65,
+	66,67,67,68,69,70,70,71,
+	72,72,73,74,75,75,76,77,
+	78,78,79,80,81,81,82,83,
+	84,84,85,86,87,87,88,89,
+	90,90,91,92,93,93,94,95,
+	96,97,97,98,99,100,100,101,
+	102,103,104,104,105,106,107,108,
+	108,109,110,111,112,112,113,114,
+	115,116,116,117,118,119,119,120,
+	121,122,123,123,124,125,126,127};
 
 void Initialize() {
-	sprintf(String, "initialize \n");
-	print(String);
-	_delay_ms(100);
 	cli(); //disable global interrupts for setup
 	
-	//pin 8 is the input - echo - B0  (PB0=ICP1=PCINT0 -input capture for timer 1)
-	//pin 7 is the output - trig - D7
-	
-	DDRD |= (1<<DDD7); //set to output pin
-	PORTD &= ~(1<<PORTD7); // set initially to low
-	
-	DDRB &= ~(1<<DDB0); //set to input pin - pin 8 for reading echo
-	PORTB &= ~(1<<PORTB0);
-	
-	//button setup
-	DDRB &= ~(1<<DDB1); //set to input pin - pin 9 for reading button
-	PORTB |= (1<<PORTB1); //enable pull up resistor
-	PCICR |= (1<<PCIE0); //enable PCINT1 pin change interrupt
-	PCMSK0 |= (1<<PCINT1); //enable trigger for PCINT1
-	
-	//timer 1 setup with pre scaler of 8
-	TCCR1B &= ~(1<<CS10);
-	TCCR1B |= (1<<CS11);
-	TCCR1B &= ~(1<<CS12);
-	
-	//set timer to normal
-	TCCR1A &= ~(1<<WGM10);
-	TCCR1A &= ~(1<<WGM11);
-	TCCR1B &= ~(1<<WGM12);
-	TCCR1B &= ~(1<<WGM13);
-	
-	TCCR1B |= (1<<ICES1);//looking for rising edge
-	TIFR1 |= (1<<ICF1);//clear interrupt flag
-	TIMSK1 |= (1<<ICIE1);//enable input capture interrupt
-	
-	TCCR1A |= (1<<COM1A0);
-	TCCR1A &= ~(1<<COM1A1);
-	
-	//buzzer and PWM stuff below
 	DDRD |= (1<<DDD6); //set buzzer to output pin (PD6 = OC0A)
 	PORTD |= (1<<PORTD6); // set initially to low
-	DDRD |= (1<<DDD5); //set OC0B to output pin
-	PORTD |= (1<<PORTD5); // set initially to low
 	
 	//timer 0 setup, sets pre scaler to 64
 	TCCR0B |= (1<<CS00);
@@ -109,104 +160,29 @@ void Initialize() {
 	//toggle OC1A on compare match
 	TCCR0A |= (1<<COM0A0);
 	TCCR0A &= ~(1<<COM0A1);
-	//OC0B = 1 on compare match when up counting; OC0B = 0 on comp match when down counting
-	//OC0B = PCINT21 - PD5
-	TCCR0A |= (1<<COM0B0);
-	TCCR0A |= (1<<COM0B1);
+  
+	OCR0A = 142;//16*10^6 / 64 / (440*2*2) = 142
 	
-	OCR0A = 30;
-	OCR0B = OCR0A * (1.0 - (45/100.0));//initial duty cycle of 45
+	//PC setup
+	//DDRB &= ~(1<<DDB1); //set to input pin - pin 9 for reading button
+	//PORTB |= (1<<PORTB1); //enable pull up resistor
+	//OCR0A is pin 6
+	PCICR |= (1<<PCIE2); //enable PCINT1 pin change interrupt
+	PCMSK2 |= (1<<PCINT22); //enable trigger for PCINT1
 	
-	//ADC photoresistor setup
-	PRR &= ~(1<<PRADC); //clear power reduction for adc
-	//select Vref = AVcc
-	ADMUX |= (1<<REFS0);
-	ADMUX &= ~(1<<REFS1);
-	//set the ADC clock div by 128
-	ADCSRA |= (1<<ADPS0);
-	ADCSRA |= (1<<ADPS1);
-	ADCSRA |= (1<<ADPS2);
-	//select channel 0
-	ADMUX &= ~(1<<MUX0);
-	ADMUX &= ~(1<<MUX1);
-	ADMUX &= ~(1<<MUX2);
-	ADMUX &= ~(1<<MUX3);
-	//set to free running
-	ADCSRA |= (1<<ADATE); //autotriggering of ADC
-	ADCSRB &= ~(1<<ADTS0); //free running mode ADTS[2:0]==000
-	ADCSRB &= ~(1<<ADTS1);
-	ADCSRB &= ~(1<<ADTS2);
-	//disable digital input buffer on ADC pin
-	DIDR0 |= (1<<ADC0D);
-	//enable ADC
-	ADCSRA |= (1<<ADEN);
-	//enable ADC interrupt
-	ADCSRA |= (1<<ADIE);
-	//start conversion
-	ADCSRA |= (1<<ADSC);
-
 	sei();//enable global interrupts
 }
-ISR(TIMER1_CAPT_vect) {
-	if (mode != 0) {
-		x ++;
-		if (TCCR1B & (1 << ICES1)) {
-			TCNT1 = 0;
-			//timeStart = TCNT1;
-			numOverflows = 0;
-			mode = 1;
-			_delay_us(10);
-			TCCR1B &= ~(1<<ICES1);//looking for falling edge
-		} else {
-			timeEnd = TCNT1;
-			mode = 0;
-			TCCR1B |= (1<<ICES1);//rising edge
+ISR(PCINT2_vect) { // button change
+	if (PIND & (1<<PIND6)) { //button is high
+		OCR0A = sine[x];
+		x++;
+		if (x >= 1000) {
+			x = 0;
 		}
-	} else {
-		x--;
-	}
-	
-}
-ISR(TIMER1_OVF_vect) {
-	numOverflows++;
-}
-ISR(PCINT0_vect) { // button change
-	if (PINB & (1<<PINB1)) { //button is high
-		discrete = !discrete; //toggle discrete and continuous modes
-		//discrete = 0;
 	}
 }
-int main(void)
-{
-	print_init();
+
+int main(void) {
 	Initialize();
-	while (1) {
-		sprintf(String, "%u mode \n", mode);
-		/print(String);
-		if (mode == 0) {
-			PORTD |= (1<<PORTD7);//output high
-			_delay_us(5000);
-			PORTD &= ~(1<<PORTD7);//output low
-			mode = 1;
-		}
-		double distance = (64 * (timeEnd) * 8) / 1024 / 58;
-		sprintf(String, "%u :distance \n", (int)distance);
-		print(String);
-		int converted = 0;
-		if (discrete == 0) {
-			converted = (int)(distance * 0.4285 + 31.71);
-		} else {
-			int pos_in_range = (int)(distance/70*8);
-			if (pos_in_range >= 8) {
-				pos_in_range = 7;
-			}
-			converted = discrete_freqs[pos_in_range];
-		}
-		sprintf(String, "%u :num \n", converted);
-		print(String);
-		OCR0A = converted;
-		sprintf(String, "%u :discrete? \n", (int)discrete);
-		print(String);
-		_delay_ms(1000);
-	}
+	while (1);
 }
